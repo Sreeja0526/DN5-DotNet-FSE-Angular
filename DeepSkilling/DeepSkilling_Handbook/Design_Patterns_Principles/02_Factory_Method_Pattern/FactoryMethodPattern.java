d230b6ad284a48f6cc42fb7763874bba37c184f1
Sreeja0526 <231fa04077@gmail.com>
2026-06-22 21:30:22 +0530

    Add Factory Method Pattern exercise

---
+++ DeepSkilling/DeepSkilling_Handbook/Design_Patterns_Principles/02_Factory_Method_Pattern/FactoryMethodPattern.java
@@ -0,0 +1,45 @@
+interface Document {
+    void open();
+}
+
+class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening Word Document");
+    }
+}
+
+class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening PDF Document");
+    }
+}
+
+abstract class DocumentFactory {
+    public abstract Document createDocument();
+}
+
+class WordDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+class PdfDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+}
+
+public class FactoryMethodPattern {
+
+    public static void main(String[] args) {
+
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+    }
+}
---
+++ DeepSkilling/DeepSkilling_Handbook/Design_Patterns_Principles/02_Factory_Method_Pattern/README.md
@@ -0,0 +1,24 @@
+# Factory Method Pattern
+
+## Objective
+
+Implement the Factory Method Design Pattern.
+
+## Description
+
+The Factory Method Pattern provides an interface for creating objects while allowing subclasses to decide which class to instantiate.
+
+## Files
+
+* FactoryMethodPattern.java
+
+## Expected Output
+
+Opening Word Document
+Opening PDF Document
+
+## Benefits
+
+* Promotes loose coupling
+* Easy to extend with new document types
+* Follows Open/Closed Principle
